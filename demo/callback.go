44d80ee2e651b7a058d3a613bd7e364873e1208e
zsm <zhangshimiande@gmail.com>
2023-02-06 11:24:56 +0800

    update demo

--- demo/callback.go
+++ demo/callback.go
@@ -23,8 +23,8 @@ func CallbackMain() {
 	})
 	e.POST("/callback/shop", func(c echo.Context) error {
 		// 打印微信回调事件请求体，便于调试
-		//requestBody, _ := ioutil.ReadAll(c.Request().Body)
-		//fmt.Printf("channels post : uri=%s, req=%s\n", c.Request().RequestURI, string(requestBody))
+		// requestBody, _ := ioutil.ReadAll(c.Request().Body)
+		// fmt.Printf("channels post : uri=%s, req=%s\n", c.Request().RequestURI, string(requestBody))
 
 		_ = CallbackRepo.HandleShopPostReqShouest(c)
 		return c.String(200, "success")
@@ -37,8 +37,8 @@ func CallbackMain() {
 	})
 	e.POST("/callback/window", func(c echo.Context) error {
 		// 打印微信回调事件请求体，便于调试
-		//requestBody, _ := ioutil.ReadAll(c.Request().Body)
-		//fmt.Printf("channels post : uri=%s, req=%s\n", c.Request().RequestURI, string(requestBody))
+		// requestBody, _ := ioutil.ReadAll(c.Request().Body)
+		// fmt.Printf("channels post : uri=%s, req=%s\n", c.Request().RequestURI, string(requestBody))
 
 		_ = CallbackRepo.HandleWindowPostRequest(c)
 		return c.String(200, "success")
