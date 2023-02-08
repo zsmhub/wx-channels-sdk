e015a93367559f51141680f931d8be7c591b06bf
zsm <zhangshimiande@gmail.com>
2023-02-08 09:29:55 +0800

    add callback

---
+++ callbacks/event商品编辑.go
@@ -0,0 +1,42 @@
+package callbacks
+
+import (
+	"encoding/json"
+)
+
+// 商品编辑
+// 文档: 暂无
+
+func init() {
+	supportCallback(EventProductSpuUpdate{})
+}
+
+type EventProductSpuUpdate struct {
+	ToUserName       string `json:"ToUserName"`
+	FromUserName     string `json:"FromUserName"`
+	CreateTime       int    `json:"CreateTime"`
+	MsgType          string `json:"MsgType"`
+	Event            string `json:"Event"`
+	ProductSpuUpdate struct {
+		ProductId int64 `json:"product_id"`
+		Status    int   `json:"status"`
+	} `json:"ProductSpuUpdate"`
+}
+
+func (EventProductSpuUpdate) GetMessageType() string {
+	return "event"
+}
+
+func (EventProductSpuUpdate) GetEventType() string {
+	return "product_spu_update"
+}
+
+func (m EventProductSpuUpdate) GetTypeKey() string {
+	return m.GetMessageType() + ":" + m.GetEventType()
+}
+
+func (EventProductSpuUpdate) ParseFromJson(data []byte) (CallBackExtraInfoInterface, error) {
+	var temp EventProductSpuUpdate
+	err := json.Unmarshal(data, &temp)
+	return temp, err
+}
